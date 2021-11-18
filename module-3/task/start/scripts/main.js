23bd2f2962bf819a39fbc13a94213bebfb82ab98
nathanielpells <93220158+nathanielpells@users.noreply.github.com>
2021-11-18 20:57:30 +0000

    task 8 and 9 calc

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -54,9 +54,111 @@ function productDiscount(){
         sum = price * quantity; 
         let discount = discountAmount * quantity;
         sum = sum - discount;
-    }else{
+    } else {
         sum = price * quantity;
     }
     console.log(sum);
 }
 productDiscount();
+
+
+
+
+
+
+
+
+
+
+
+
+//Create a function called drink order that takes 2 parameters {size} and {drink}
+function drinkOrder(size,drink){
+    // VALIDATE the variables passed through
+    if(size != 'small' && size != 'medium' && size != 'large'){
+        console.log('You have ordered a size we don\'t sell')
+        return;
+    }
+    let message = 'You have ordered a ' + size;
+    //SWITCH {drink}
+    switch(drink){
+        case 'cola':
+            message += ' Coca-Cola';
+            break;
+        case 'lemon':
+            message += ' Schwepps';
+            break;
+        case 'orange':
+            message += ' Tango';
+            break;
+        default:
+            message = 'You have ordered a size we don\'t sell';
+            break;
+    }
+    //RETURN message
+    console.log(message);
+}
+
+drinkOrder('grande');
+drinkOrder('small','cola');
+drinkOrder('medium','orange');
+drinkOrder('large','lemon')
+
+
+//FUNCTION calculator {num1} {num2}{operator}
+function calculator(num1,num2,operator){
+    //VALIDATE all data
+    if(isNaNValidator(num1)){
+       return 'Number 1 is not a number';
+    }
+    if(isNaNValidator(num2)){
+        return 'Number 2 is not a number';
+    } 
+    //SWITCH {operator}
+    let sum;
+    switch(operator){
+        case '+':
+            sum = num1 + num2;
+            break;
+        case '-':
+            sum = num1 - num2;
+            break;
+        case '/':
+            sum = num1 / num2;
+            break;
+        case '*':
+            sum = num1 * num2;
+            break;
+        case '%':
+            sum = num1 % num2;
+            break;
+        default:
+            return 'Operator is not recognised';
+    }
+    //RETRUN sum
+    return sum;
+ 
+}
+
+function isNaNValidator(number){
+    return isNaN(number)
+
+}
+
+
+let sum1=calculator(10,2,'+');
+let sum2=calculator(10,2,'-');
+let sum3=calculator(10,2,'/');
+let sum4=calculator(10,2,'*');
+let sum5=calculator(10,2,'%');
+let sum6=calculator(1,'nathaniel');
+let sum7=calculator('nathaniel');
+let sum8=calculator(1,1,'nathaniel');
+console.log(sum1);
+console.log(sum2);
+console.log(sum3);
+console.log(sum4);
+console.log(sum5);
+console.log(sum6);
+console.log(sum7);
+console.log(sum8);
